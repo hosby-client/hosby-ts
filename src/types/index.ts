131aebc0511adf1685e2f148b6a9ef0b8ccc351f
Hosby team core <support@hosby.io>
2024-12-22 12:01:10 +0000

    Add types Base

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,8 @@
 export type Criteria<T> = Partial<Record<keyof T, any>>;
+
+export type TBaseClient = {
+    baseURL: string;
+    apiKey: string;
+    headers?: Record<string, string>;
+    [key: string]: any;
+}
